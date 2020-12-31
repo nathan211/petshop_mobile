4b1390e8f882c61ec6bc31df41a4904209944ca2
Viet Phi Nguyen <vietphi99dh@gmail.com>
2020-12-31 19:15:53 +0700

    add ui filter products modal

--- app/components/lists/FilterItem.js
+++ app/components/lists/FilterItem.js
@@ -1,34 +1,30 @@
 import React from 'react'
 import { StyleSheet, TouchableWithoutFeedback, View } from 'react-native'
 
-import colors from '../config/colors';
-import Text from '../components/Text';
+import colors from '../../config/colors';
+import Text from '../../components/Text';
 
-export default function TimeItem({ isSelected, onPress, label, disable }) {
+export default function FilterItem({ isSelected, onPress, label, disable }) {
     return (
         <TouchableWithoutFeedback onPress={onPress}>
             { isSelected ?
-            <Text customStyle={[styles.timeItemSelected, disable]}>{label}</Text> :
-            <Text customStyle={[styles.timeItem, disable]}>{label}</Text>}
+            <Text customStyle={[styles.itemSelected, disable]}>{label}</Text> :
+            <Text customStyle={[styles.item, disable]}>{label}</Text>}
         </TouchableWithoutFeedback> 
     )
 }
 
 const styles = StyleSheet.create({
-    timeItemSelected: {
+    itemSelected: {
         backgroundColor: colors.secondary,
         color: colors.white,
-        width: 65,
-        height: 35,
         textAlign: 'center',
         padding: 5,
         borderRadius: 25,
         margin: 5
     },
-    timeItem: {
+    item: {
         backgroundColor: colors.lightPink,
-        width: 65,
-        height: 35,
         textAlign: 'center',
         padding: 5,
         borderRadius: 25,
--- app/screens/ProductsScreen.js
+++ app/screens/ProductsScreen.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, ScrollView, TouchableWithoutFeedback, FlatList } from 'react-native';
+import { StyleSheet, View, ScrollView, TouchableWithoutFeedback, Modal } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
+import Button from '../components/Button';
 import colors from '../config/colors';
 import Card from '../components/Card';
 import SearchBar from '../components/SearchBar';
@@ -9,9 +10,16 @@ import ShoppingCart from '../components/ShoppingCart';
 import productApi from '../api/product';
 import numberFormatter from '../utilities/numberFormatter';
 import Text from '../components/Text';
+import FilterItem from '../components/lists/FilterItem';
 
 export default function CategoryScreen({ navigation, route }) {
     const [listOfProducts, setListOfProducts] = useState([]);
+    const [modalVisible, setModalVisible] = useState(false);
+    const [isSelected, setIsSelected] = useState(null);
+    const [filterItems, setFilterItems] = useState([
+        { label:'Thấp đến cao', value: 1},
+        { label:'Cao đến thấp', value: 2}
+    ]);
 
     useEffect(() => {
         getFilteredProductsByCategory();
@@ -29,7 +37,15 @@ export default function CategoryScreen({ navigation, route }) {
     }
 
     const handleShowFilterModal = () => {
-        
+        setModalVisible(!modalVisible);
+    }
+
+    const handleCloseModal = () => {
+        setModalVisible(!modalVisible);
+    }
+
+    const handleChooseFilterItem = (item) => {
+        setIsSelected(item.value);
     }
 
     return (
@@ -49,20 +65,21 @@ export default function CategoryScreen({ navigation, route }) {
                 <SearchBar customStyle={styles.customSearchBarStyle} />
                 <ShoppingCart onPress={() => navigation.navigate('ShoppingCart')} />
             </View>
+            <View style={styles.filterContainer}>
+                <Text customStyle={styles.title}>Tất cả sản phẩm: </Text>
+                <TouchableWithoutFeedback onPress={handleShowFilterModal}>
+                    <View style={styles.filterWrapper}>
+                        <Text customStyle={styles.filterText}>Bộ lọc</Text>
+                        <Icon 
+                            name='filter'
+                            size={23}
+                            color={colors.brown}
+                        />
+                        
+                    </View>
+                </TouchableWithoutFeedback>
+            </View>
             <ScrollView>
-                <View style={styles.filterContainer}>
-                    <Text customStyle={styles.title}>Tất cả sản phẩm: </Text>
-                    <TouchableWithoutFeedback onPress={handleShowFilterModal}>
-                        <View style={styles.filterWrapper}>
-                            <Text customStyle={styles.filterText}>Bộ lọc</Text>
-                            <Icon 
-                                name='filter'
-                                size={23}
-                                color={colors.brown}
-                            />
-                        </View>
-                    </TouchableWithoutFeedback>
-                </View>
                 <View  style={styles.cardContainer}>
                 {
                     listOfProducts.map((item) => (
@@ -78,17 +95,69 @@ export default function CategoryScreen({ navigation, route }) {
                 }
                 </View>
             </ScrollView>
+            <Modal
+                animationType="fade"
+                transparent={true}
+                visible={modalVisible}
+                onRequestClose={() => {
+                Alert.alert("Modal has been closed.");
+                }}
+            >
+                <View style={styles.modalContainer}>
+                    <View style={styles.modalHeader}>
+                        <TouchableWithoutFeedback onPress={handleCloseModal}>
+                            <View style={styles.closeModalIcon}>
+                                <Icon
+                                    name='window-close'
+                                    size={25}
+                                    color={colors.white}
+                                />
+                            </View>
+                        </TouchableWithoutFeedback>
+                        <View style={styles.modalHeaderTitleContainer}>
+                            <Text customStyle={styles.modalHeaderTitle}>Lọc sản phẩm</Text>
+                        </View>
+                    </View>
+                    <View style={styles.modalContent}>
+                        <View style={styles.filterByPrice}>
+                            <Text customStyle={styles.filterTitle}>Giá: </Text>
+                            <View style={styles.priceWrapper}>
+                                {
+                                    filterItems.map((item, key) => (
+                                        <FilterItem 
+                                            label={item.label}
+                                            onPress={() => handleChooseFilterItem(item)}
+                                            isSelected={isSelected === item ? true : false}
+                                            key={key}
+                                        />
+                                    ))
+                                }
+                            </View>
+                        </View>
+                    </View>
+                    <View style={styles.buttonContainer}>
+                        <Button 
+                            title='Lọc' 
+                            color='dark'
+                            customTitleStyle={{color: colors.white}}
+                        />
+                    </View>
+                </View>
+            </Modal>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
+    buttonContainer: {
+        padding: 5,
+    },
     container: {},
     cardContainer: {
         paddingHorizontal: 20,
         marginBottom: 70,
         flexDirection: 'row',
-        flexWrap: 'wrap'
+        flexWrap: 'wrap',
     },
     header: {
         height: 60,
@@ -96,6 +165,8 @@ const styles = StyleSheet.create({
         backgroundColor: colors.pink,
         padding: 10,
         alignItems: 'center'
+    },
+    closeModalIcon: {
     },
     customSearchBarStyle: {
         flex: 1,
@@ -119,17 +190,48 @@ const styles = StyleSheet.create({
     filterContainer: {
         flexDirection: 'row',
         justifyContent: 'space-between',
-        padding: 10,
+        alignItems: 'center',
+        padding: 5,
         backgroundColor: colors.white,
+        zIndex: 1,
     },
     filterWrapper: {
         flexDirection: 'row',
-        justifyContent: 'center',
+        backgroundColor: colors.light,
+        padding: 5,
+        borderRadius: 5,
     },
     filterText: {
         fontWeight: 'bold',
     },
     title: {
         fontWeight: 'bold'
+    },
+    modalContent: {
+        flex: 1,
+    },
+    modalContainer: {
+        width: '70%',
+        height: '100%',
+        backgroundColor: colors.white,
+        alignSelf: 'flex-end',
+        elevation: 100,
+    },
+    modalHeader: {
+        flexDirection: 'row',
+        padding: 5,
+        backgroundColor: colors.pink,
+    },
+    modalHeaderTitleContainer: {
+        flex: 1,
+        alignItems: 'center',
+    },
+    modalHeaderTitle: {
+        color: colors.white,
+    },
+    filterTitle: {
+        fontWeight: 'bold',
+        marginTop: 10,
+        marginLeft: 10,
     }
 });
